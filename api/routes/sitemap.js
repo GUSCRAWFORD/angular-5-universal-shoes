1fc23651293cee13b9925e4541b8bac716346bf9
Gus Crawford <gus.crawford@envisageny.com>
2018-06-29 18:26:18 -0400

    add example request in comments

--- api/routes/sitemap.js
+++ api/routes/sitemap.js
@@ -46,3 +46,26 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+/*
+
+e.x.
+GET http://localhost:4000/shoes/detail/baj1
+<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Blue Air Jordan</title><base href="/"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" type="image/x-icon" href="favicon.ico"><link href="styles.9c0ad738f18adc3d19ed.bundle.css" rel="stylesheet"><style ng-transition="seo-poc"></style><style ng-transition="seo-poc">p[_ngcontent-c1]{display:inline-block}</style><meta name="keywords" content="shoe,jordan,basketball"></head><body><app-root _nghost-c0="" ng-version="5.2.11">
+<div _ngcontent-c0="" style="text-align:center">
+  <h1 _ngcontent-c0="">
+    Welcome to Shoes!
+  </h1>
+</div>
+<h2 _ngcontent-c0="">Here are some links to help you start: </h2>
+<ul _ngcontent-c0="">
+  <li _ngcontent-c0="">
+    <h2 _ngcontent-c0=""><a _ngcontent-c0="" href="/shoes/catalog">Shoe Catalog</a></h2>
+  </li>
+</ul>
+<router-outlet _ngcontent-c0=""></router-outlet><app-detail _nghost-c1=""><img _ngcontent-c1="" alt="" style="max-width:250px;" src="https://www.flightclub.com/media/catalog/product/0/1/012597_1.jpg">
+<p _ngcontent-c1="">
+  Blue Air Jordan
+</p>
+</app-detail></app-root><script type="text/javascript" src="inline.bf47e1e63f780bc5f735.bundle.js"></script><script type="text/javascript" src="polyfills.4525a1efb8045a6cdc51.bundle.js"></script><script type="text/javascript" src="main.fb23a586605c5ee324f2.bundle.js"></script></body></html>
+*/
