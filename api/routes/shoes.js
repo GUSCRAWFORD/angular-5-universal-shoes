60dd70de7f0d53b891df4b44eac98bfa84280653
Gus Crawford <gus.crawford@envisageny.com>
2018-06-29 18:18:34 -0400

    remove inaccurate comments

--- api/routes/shoes.js
+++ api/routes/shoes.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var shoes = require('../shoes');
-/* GET users listing. */
+
 router.get('/:shoeId', function(req, res, next) {
   res.send(shoes.find(shoe=>shoe.id===req.params.shoeId));
 });
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
   res.send(shoes);
 });
